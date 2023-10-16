d8cb82f3ec7266aa2f2686e1a897874b33760fe0
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-10-16 15:57:34 +0530

    signup page responsive

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -1,5 +1,13 @@
-import {Alert,Box,Button,Link,Paper,Stack,TextField,
-Typography} from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Link,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useState } from "react";
 import VisibilityIcon from "@mui/icons-material/Visibility";
@@ -11,163 +19,222 @@ import { useNavigate } from "react-router-dom";
 import { createUserCart } from "../slices/CartSlice";
 import { Signup } from "../assets";
 
-
 const Register = () => {
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [confirm, setConfirm] = useState("");
-const [password_err, setPasswordErr] = useState(null);
-const [confirm_err, setConfirmErr] = useState(null);
-const [show, setShow] = useState(false);
-const[ show2,setShow2]=useState(false);
-const dispatch=useDispatch();
-const [error,setError]=useState(null);
-const navigate= useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [password_err, setPasswordErr] = useState(null);
+  const [confirm_err, setConfirmErr] = useState(null);
+  const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const dispatch = useDispatch();
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-const {userInfo}=useSelector(state=>state.user);
+  const { userInfo } = useSelector((state) => state.user);
 
-useEffect(()=>{
-  userInfo && dispatch(createUserCart(userInfo._id));
-  userInfo && navigate("/")
-},[userInfo]);
+  useEffect(() => {
+    userInfo && dispatch(createUserCart(userInfo._id));
+    userInfo && navigate("/");
+  }, [userInfo]);
 
-
-const handlePassword = (value) => {
-  setPassword(value);
-  if (!value.match(/[A-Z]/)) {
-    setPasswordErr("must contain capital letter");
-  }else if(!value.match(/[0-9]/)){
+  const handlePassword = (value) => {
+    setPassword(value);
+    if (!value.match(/[A-Z]/)) {
+      setPasswordErr("must contain capital letter");
+    } else if (!value.match(/[0-9]/)) {
       setPasswordErr("must contain a number");
-  }
-  else if(!value.match(/[@#$%*]/)){
+    } else if (!value.match(/[@#$%*]/)) {
       setPasswordErr("must contain ne of these(@,#,$,%,*)");
-  }
-  else if(value.length<8){
+    } else if (value.length < 8) {
       setPasswordErr("should have atleast 8 character");
-  }
-  else{setPasswordErr(null);
-  return true;}
-  return false;
-};
+    } else {
+      setPasswordErr(null);
+      return true;
+    }
+    return false;
+  };
 
-const handleConfirm= (value)=>{
-  setConfirm(value);
-  if(!(value=== password)){
+  const handleConfirm = (value) => {
+    setConfirm(value);
+    if (!(value === password)) {
       setConfirmErr("Password should mach");
-  }else {
+    } else {
       setConfirmErr(null);
       return true;
-  }
-  return false;
+    }
+    return false;
+  };
 
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name !== "" && handlePassword(password) && handleConfirm(confirm)) {
+      dispatch(registerUser({ name, email, password }));
+      setError(null);
+    } else {
+      setError("Invalid Data");
+    }
+  };
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if(name !== ""&& handlePassword(password) && handleConfirm(confirm)){
-  dispatch(registerUser({name,email,password}));
-  setError(null);
-}else{
-  setError("Invalid Data");
-
-}
-}
-
-return (
-  <Stack justifyContent={"center"} alignItems={"center"}>
-    <form onSubmit={handleSubmit}>
-      <Paper
-        elevation={4}
-        sx={{
-          width: "60vw",
-          height: "70vh",
-          margin: 2,
-          padding: 8,
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <Box sx={{height:"100%",width:"50%"}}>
-        <img src={Signup} alt="" style={{height:"100%"}} />
-        </Box>
-        <Box sx={{height:"100%",width:"50%",display:"flex",flexDirection:"column",justifyContent:"space-between"}}>
-        <Typography sx={{textAlign:"center"}} variant="h5">
-          Register
-        </Typography>
-        {error && <Alert severity="error">{error}</Alert>}
-        <TextField
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          label="Username"
-          variant="standard"
-          fullWidth
-        />
-        <TextField
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          label="Email"
-          type={"email"}
-          variant="standard"
-          fullWidth
-        />
-        <Box sx={{ display: "flex", position: "relative" }}>
-          <TextField
-            error={password_err}
-            value={password}
-            onChange={(e) => handlePassword(e.target.value)}
-            label="Password"
-            type={show ? "text": "password"}
-            variant="standard"
-            helperText={password_err && password_err}
-            fullWidth
-          />
-          {show ? (
-            <VisibilityOffIcon
-              sx={{ cursor: "pointer", position: "absolute", right: "0%", bottom:"40%" }}
-              onClick={()=>setShow(!show)}
+  return (
+    <Stack justifyContent={"center"} alignItems={"center"}>
+      <form onSubmit={handleSubmit}>
+        <Paper
+          elevation={4}
+          sx={{
+            minWidth: "60vw",
+            minHeight: "70vh",
+            margin: 2,
+            padding: {
+              xs: 4,
+              sm: 8,
+            },
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: {
+              xs: "column",
+              lg: "row",
+            },
+            gap: 5,
+          }}
+        >
+          <Box
+            sx={{
+              height: {
+                xs: "300px",
+                md: "380px",
+                lg: "450px",
+              },
+              width: {
+                xs: "100%",
+                lg: "50%",
+              },
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src={Signup} alt="" style={{ height: "100%" }} />
+          </Box>
+          <Box
+            sx={{
+              height: "100%",
+              width: {
+                xs: "100%",
+                lg: "50%",
+              },
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              gap: 3,
+            }}
+          >
+            <Typography sx={{ textAlign: "center" }} variant="h5">
+              Register
+            </Typography>
+            {error && <Alert severity="error">{error}</Alert>}
+            <TextField
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              label="Username"
+              variant="standard"
+              fullWidth
             />
-          ) : (
-            <VisibilityIcon
-              sx={{ cursor: "pointer", position: "absolute", right: "0%",bottom:"40%" }}
-              onClick={() => setShow(!show)}
+            <TextField
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              label="Email"
+              type={"email"}
+              variant="standard"
+              fullWidth
             />
-          )}
-        </Box>
-        <Box sx={{ display: "flex", position: "relative" }}>
-        <TextField error={confirm_err}
-          value={confirm}
-          onChange={(e) => handleConfirm(e.target.value)}
-          label="Confirm Password"
-          type={show2 ? "text":"password"}
-          variant="standard"
-          fullWidth
-          helperText={confirm_err && confirm_err}
-        />
-        {show2 ? (
-            <VisibilityOffIcon
-              sx={{ cursor: "pointer", position: "absolute", right: "0%",bottom:"40%" }}
-              onClick={()=>setShow2(!show2)}
-            />
-          ) : (
-            <VisibilityIcon
-              sx={{ cursor: "pointer", position: "absolute", right: "0%" , bottom:"40%" }}
-              onClick={() => setShow2(!show2)}
-            />
-          )}
-        </Box>
+            <Box sx={{ display: "flex", position: "relative" }}>
+              <TextField
+                error={password_err}
+                value={password}
+                onChange={(e) => handlePassword(e.target.value)}
+                label="Password"
+                type={show ? "text" : "password"}
+                variant="standard"
+                helperText={password_err && password_err}
+                fullWidth
+              />
+              {show ? (
+                <VisibilityOffIcon
+                  sx={{
+                    cursor: "pointer",
+                    position: "absolute",
+                    right: "0%",
+                    bottom: "40%",
+                  }}
+                  onClick={() => setShow(!show)}
+                />
+              ) : (
+                <VisibilityIcon
+                  sx={{
+                    cursor: "pointer",
+                    position: "absolute",
+                    right: "0%",
+                    bottom: "40%",
+                  }}
+                  onClick={() => setShow(!show)}
+                />
+              )}
+            </Box>
+            <Box sx={{ display: "flex", position: "relative" }}>
+              <TextField
+                error={confirm_err}
+                value={confirm}
+                onChange={(e) => handleConfirm(e.target.value)}
+                label="Confirm Password"
+                type={show2 ? "text" : "password"}
+                variant="standard"
+                fullWidth
+                helperText={confirm_err && confirm_err}
+              />
+              {show2 ? (
+                <VisibilityOffIcon
+                  sx={{
+                    cursor: "pointer",
+                    position: "absolute",
+                    right: "0%",
+                    bottom: "40%",
+                  }}
+                  onClick={() => setShow2(!show2)}
+                />
+              ) : (
+                <VisibilityIcon
+                  sx={{
+                    cursor: "pointer",
+                    position: "absolute",
+                    right: "0%",
+                    bottom: "40%",
+                  }}
+                  onClick={() => setShow2(!show2)}
+                />
+              )}
+            </Box>
 
-        <Button type="submit" variant="contained" fullWidth>
-          Submit
-        </Button>
-        <Typography>
-          Have an account? <Link sx={{cursor:"pointer"}} onClick={()=>navigate("/login")} underline="always"> Login here</Link>
-        </Typography>
-        </Box>
-      </Paper>
-    </form>
-  </Stack>
-);
+            <Button type="submit" variant="contained" fullWidth>
+              Submit
+            </Button>
+            <Typography>
+              Have an account?{" "}
+              <Link
+                sx={{ cursor: "pointer" }}
+                onClick={() => navigate("/login")}
+                underline="always"
+              >
+                {" "}
+                Login here
+              </Link>
+            </Typography>
+          </Box>
+        </Paper>
+      </form>
+    </Stack>
+  );
 };
 
 export default Register;
